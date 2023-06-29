7525fc1d135c2b95a170504b48db8ae13c305fa1
sanket bodke <sanketbodke26@gmail.com>
2023-06-29 22:35:30 +0530

    dark mode add

--- main.js
+++ main.js
@@ -23,3 +23,25 @@ $(document).ready(function() {
     })
 
 });
+
+function darkMode() {
+    $('body').toggleClass('dark-mode');
+    $('.table tr:nth-child(even)').css({
+        'background-color': '#fff',
+        'color': '#000',
+    })
+    $('.table tr:nth-child(4)').css({
+        'background-color': '#5bb9c0',
+        'color': '#fff',
+    })
+    $('.main_content .main_navbar .dark_mode_icon .bx-moon').click(function() {
+        $(this).css('display', 'none');
+        $('.main_content .main_navbar .dark_mode_icon .bx-sun').css('display', 'block');
+    });
+
+    $('.main_content .main_navbar .dark_mode_icon .bx-sun').click(function() {
+        $(this).css('display', 'none');
+        $('.main_content .main_navbar .dark_mode_icon .bx-moon').css('display', 'block');
+    });
+
+}
